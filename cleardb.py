63c771be1402df64fdf5fbe0417dad346e0c23b5
Thales Gomes <thales3238junior@gmail.com>
2020-02-05 15:03:11 -0400

    Atualizando arquivos de instalação. 05.02.2020.

--- cleardb.py
+++ cleardb.py
@@ -1,27 +1,41 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from app import app, db
 from app.models import User
 import random
 import os
+import sqlite3
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-UserData = User.query.all()
-db.session.delete(UserData)
-db.session.commit()
+#TRUCATE na tabela
+directoryDB = 'app/app.db'
+conexao = sqlite3.connect(directoryDB)
+cursor = conexao.cursor()
+cursor.execute('DELETE FROM user;')
+conexao.commit()
+cursor.close()
+conexao.close()
 
 #numero aleatório para senha.
 num = str(random.randrange(10000)*10000)
 #novo admin.
+name = "Administrador"
 user = 'admin'
 email = 'your.email@a.com'
-password = 'vs'+num+'dynamics
-dir = os.path.expanduser('~') + '/Visualdynamics/login.txt'
-info = open(dir,"w")
+password = 'vs'+num+'dynamics'
+
+#anota o usuario e senha gerado automaticamente
+directory = basedir+'/login.txt'
+info = open(directory,'w')
 usuario = 'username: '+user+'\n'
 senha = 'password: '+password
 info.write(usuario)
 info.write(senha)
-new = User(username=user,email=email)
+info.close()
+
+#cria usuario no banco
+new = User(name=name,username=user,email=email,register='True')
 new.set_password(password)
 db.session.add(new)
 db.session.commit()
-
--- install.sh
+++ install.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 #script sh install Visual Dynamics
-#install python3.7 e pip
-sudo apt install python3.7
-sudo apt install python3-pip
-#install grace
-sudo apt install grace
-#install virtualenv
-sudo pip3 install virtualenv
-#install git
-sudo apt install git
-cd app
-git update-index --assume-unchanged app.db
-cd ..
-#create venv
-virtualenv venv
-#init venv
-source venv/bin/activate
-#install requirements
-pip3 install -r requirements.txt
-python cleardb.py
-chmod +x run.sh
-#init server
-export FLASK_ENV=development
-echo "Para executar a aplicacao novamente depois desta instalacao, execute o arquivo run.sh que esta na pasta visualdynamics." 
-echo "Para finalizar a excução precione Ctrl + c"
-flask run
+echo "Atencao ao executar a instalacao os registros do banco de dados serao apagados."
+echo "Se for a primeira instalacao nao se preocupe com isso, sera gerado um novo login de admin que sera salvo no arquivo login.txt."
+echo "Caso voce já tenha o visual dynamics instalado, cuidado! Se realizar a instalacao novamente o banco de dados sera totalmente limpo."
+echo "Confirmar instalacao?(y = confirmar/n = cancelar)"
+read resp
+if [ $resp = 'y'];
+    then
+        #install python3.7 e pip
+        sudo apt install python3.7
+        sudo apt install python3-pip
+        #install grace
+        sudo apt install grace
+        #install virtualenv
+        sudo pip3 install virtualenv
+        #install git
+        sudo apt install git
+        cd app
+        git update-index --assume-unchanged app.db
+        cd ..
+        #create venv
+        virtualenv venv
+        #init venv
+        source venv/bin/activate
+        #install requirements
+        pip3 install -r requirements.txt
+        python cleardb.py
+        chmod +x run.sh
+        #init server
+        export FLASK_ENV=development
+        echo "Para executar a aplicacao novamente depois desta instalacao, execute o arquivo run.sh que esta na pasta visualdynamics." 
+        echo "Para finalizar a excução precione Ctrl + c"
+        flask run
 
+elif [ $resp = 'n'];
+    then
+        echo "Instacao cancelada."
+
+else 
+    echo "Opcao invalida."
 #End
