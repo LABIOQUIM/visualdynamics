b12be92fbe5bcb641d555b483c348105de066713
Thales Gomes <thales3238junior@gmail.com>
2020-02-10 11:58:48 -0400

    Atualizando gerador de senha. 10.02.2020.

--- clear_DataBase.py
+++ clear_DataBase.py
@@ -6,6 +6,7 @@ import random
 import os
 import sqlite3
 import hashlib
+
 #Cuidado!!! Ao executar esse script o Banco de dados exclui todos os usuarios.
 
 basedir = os.path.abspath(os.path.dirname(__file__))
