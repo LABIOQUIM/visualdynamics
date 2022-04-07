7f7564cdb150de8c26e5d418b37731426a8c0fd1
Ivo <ivoprovensi1@gmail.com>
2022-04-07 09:46:13 -0400

    feat: update to fontawesome 6 self hosted

--- compile-and-install-gromacs-2018.sh
+++ compile-and-install-gromacs-2018.sh
@@ -1,43 +1,43 @@
 if [ -f "/etc/arch-release" ]; then
 	if ls /usr/bin/gmx &> /dev/null || ls /usr/bin/gmx_d &> /dev/null; then
-		echo ">>> GROMACS já instalado"
+		echo ">>> GROMACS already installed"
 	else
 	# Enter working dir
-    cd libs/gromacs
+        cd libs/gromacs
 
-    # Unzip GROMACS source code
-    unzip gromacs.zip
+        # Unzip GROMACS source code
+        unzip gromacs.zip
 
-    # Make and prepare GROMACS build folder
-    mkdir -p build
-    cd build
-    cmake ../gromacs-2018/ \
-        -DCMAKE_INSTALL_PREFIX=/usr/ \
-        -DCMAKE_INSTALL_LIBDIR=lib \
-        -DGMX_BUILD_OWN_FFTW=off \
-        -DGMX_HWLOC=off
+        # Make and prepare GROMACS build folder
+        mkdir -p build
+        cd build
+        cmake ../gromacs-2018/ \
+            -DCMAKE_INSTALL_PREFIX=/usr/ \
+            -DCMAKE_INSTALL_LIBDIR=lib \
+            -DGMX_BUILD_OWN_FFTW=off \
+            -DGMX_HWLOC=off
 
-    # Build GROMACS
-    make -j$(nproc)
+        # Build GROMACS
+        make -j$(nproc)
 
-    # Then check our build
-    make check
+        # Then check our build
+        make check
 
-    # Install our package
-    sudo make install
+        # Install our package
+        sudo make install
 
-    # Append GMXRC to our .zshrc or .bash_profile
+        # Append GMXRC to our .zshrc or .bash_profile
 		if ! grep -Fxq "source /usr/bin/GMXRC" ~/.zshrc; then
 				# Not written, so write
 				echo "source /usr/bin/GMXRC" >> ~/.zshrc
 		fi
 
-    # Leave working dir/go back to visualdynamics root
-    cd ../../..
+        # Leave working dir/go back to visualdynamics root
+        cd ../../..
 	fi
 else
 	if ls /usr/bin/gmx &> /dev/null || ls /usr/bin/gmx_d &> /dev/null; then
-		echo "GROMACS already installed"
+		echo ">>> GROMACS already installed"
 	else
         # Enter working dir
         cd libs/gromacs
