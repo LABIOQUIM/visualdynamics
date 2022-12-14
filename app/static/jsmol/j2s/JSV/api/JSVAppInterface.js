df49aea460905dfb6c2023466ff0eeb88bec0822
Ivo <ivopr@proton.me>
2022-12-14 16:57:19 -0400

    feat: drop jsmol in favor of ngl (#10)

--- app/static/jsmol/j2s/JSV/api/JSVAppInterface.js
+++
@@ -1,4 +0,0 @@
-Clazz.declarePackage ("JSV.api");
-Clazz.load (["JSV.api.JSVAppletInterface"], "JSV.api.JSVAppInterface", null, function () {
-Clazz.declareInterface (JSV.api, "JSVAppInterface", JSV.api.JSVAppletInterface);
-});
