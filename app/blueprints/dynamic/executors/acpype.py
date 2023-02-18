873214be014d427778d6cc0326b7029df60991ad
Ivo <ivopr@proton.me>
2023-02-18 12:18:32 -0400

    fix: acpype dynamic running completely on linux/bash commands

--- app/blueprints/dynamic/executors/acpype.py
+++ app/blueprints/dynamic/executors/acpype.py
@@ -9,8 +9,6 @@ def execute(folder, CommandsFileName, username, filename, itpname, groname, mol)
         protein_name, _ = os.path.splitext(os.path.basename(filename))
         f.write(protein_name)
 
-    #
-
     #transferir os arquivos mdp necessarios para a execução
     RunFolder = os.path.join(folder, "run") #pasta q vai rodar
     SecureMdpFolder = os.path.join(os.path.expanduser('~'),Config.MDP_LOCATION_FOLDER)
@@ -57,60 +55,6 @@ def execute(folder, CommandsFileName, username, filename, itpname, groname, mol)
                 os.remove(os.path.join(Config.UPLOAD_FOLDER, username, 'running_protein_name'))
                 os.remove(os.path.join(Config.UPLOAD_FOLDER, username, 'log_dir'))
                 return f"{l}"
-        
-        #breakpoint adicionado para possibilitar a interação com os arquivos em tempo de execução
-        if l == '#break': 
-            #cria o novo arquivo com a molecula complexada
-            #pronto 
-            
-            #procura e adiciona em um novo arquivo 
-            comando_junta_atom = 'grep -h ATOM {}_livre.pdb {} >| {}_complx.pdb'.format(mol, groname, mol)
-            with open(linux_log_file_path, "a") as f:
-                subprocess.call(comando_junta_atom, shell=True, stdin=f, stdout=f, stderr=f)
-
-            ## 
-            atomtypes = []
-            diretorio_itp = os.path.join(RunFolder, itpname)
-            with open(diretorio_itp, 'r') as f:
-                found = False
-                for line in f:
-                    if found:
-                        if line is "\n":
-                            break
-                        atomtypes.append(line)
-                    if 'atomtypes' in line:
-                        found = True
-                        atomtypes.append(line)
-
-            ## comando 1
-            comando_gerar_molecula_complexada = 'cat {}_livre.top | sed \'/forcefield\.itp\"/a\#include "{}"\' >| {}1_complx.top'.format(mol,itpname,mol)
-            with open(os.path.join(folder, "run", "logs", f"linux-commands.log"), 'a') as f:
-                subprocess.call(comando_gerar_molecula_complexada, shell=True, stdin=f, stdout=f, stderr=f)
-
-            with open(os.path.join(RunFolder, f"{mol}1_complx.top"), "r") as f:
-                with open(os.path.join(RunFolder, f"{mol}_complx.top"), "w") as f1:
-                    for line in f:
-                        f1.write(line)
-                        if 'forcefield.itp' in line:
-                            f1.write("\n")
-                            f1.writelines(atomtypes)
-
-            #acessando arquivo .itp para pegar o moleculetype
-            #pronto
-            file = open(diretorio_itp,'r')
-            file_itp = file.readlines()
-            file.close()
-            for i, text in enumerate(file_itp):
-                if text.find('moleculetype') > -1:
-                    molecula = file_itp[i + 2]
-                    molecula = molecula.split()[0]
-                    molecula = molecula + '                 1'
-                    break
-            
-            #aqui vai o echo ligand 1
-            comando_moleculetype = 'echo "{}" >> {}_complx.top'.format(molecula,mol)
-            with open(os.path.join(folder, "run", "logs", f"linux-commands.log"), 'a') as f:
-                subprocess.call(comando_moleculetype, shell=True, stdin=f, stdout=f, stderr=f)
 
     with open(os.path.join(folder, 'status'), 'w') as f:
         f.write(f"success\n")
--- app/utils/run_dynamics_command.py
+++ app/utils/run_dynamics_command.py
@@ -8,13 +8,15 @@ def run_dynamics_command(command, log_file):
     # Split the command into a list of arguments using shlex
     args = shlex.split(command)
     
+    should_use_shell = '>' in command
+
     # Use the `with` statement to open the log file
     with open(log_file, 'a') as f:
         # Check if the command contains a pipe
         if '|' in command:
             # Split the command at the pipe character
             cmd1, cmd2 = command.split('|', 1)
-            
+
             # Split the first command into a list of arguments using shlex
             args1 = shlex.split(cmd1)
             
@@ -22,10 +24,10 @@ def run_dynamics_command(command, log_file):
             args2 = shlex.split(cmd2)
             
             # Create the first subprocess using `Popen`
-            process1 = subprocess.Popen(args1, stdout=subprocess.PIPE)
+            process1 = subprocess.Popen(args1, stdout=subprocess.PIPE, stderr=f)
             
             # Create the second subprocess using `Popen` and connect the stdout and stderr to the log file
-            process2 = subprocess.Popen(args2, stdin=process1.stdout, stdout=f, stderr=f, preexec_fn=os.setsid)
+            process2 = subprocess.Popen(cmd2 if should_use_shell else args2, shell=should_use_shell, stdin=process1.stdout, stdout=f, stderr=f, preexec_fn=os.setsid)
             
             with open(os.path.join(Config.UPLOAD_FOLDER, current_user.username, "pid"), "w") as f:
                 f.write(f"{process2.pid}")
@@ -38,7 +40,7 @@ def run_dynamics_command(command, log_file):
             return (process2.pid, process2.returncode)
         else:
             # Run the command using `run` and redirect stdout and stderr to the log file
-            process = subprocess.Popen(args, stdout=f, stderr=f, preexec_fn=os.setsid)
+            process = subprocess.Popen(command if should_use_shell else args, shell=should_use_shell, stdout=f, stderr=f, preexec_fn=os.setsid)
             
             with open(os.path.join(Config.UPLOAD_FOLDER, current_user.username, "pid"), "w") as f:
                 f.write(f"{process.pid}")
