2306af0db8a241308b6b8051b8e1f420e785cb17
Ivo <ivo@vist.gg>
2023-04-12 21:27:18 -0400

    feat(server): run route now just calls celery process

--- apps/server/server/celery_tasks.py
+++ apps/server/server/celery_tasks.py
@@ -1,18 +1,39 @@
 import os
+import shutil
+from server.config import Config
 from server.utils.run_command import run_command
 from server.celery import celery
 
 
 @celery.task()
-def run_commands(folder, commands):
-    print(
-        folder,
-    )
+def run_commands(folder):
+    # Get absolute path to the folder where our default MDP files are stored
+    folder_mdp = os.path.abspath(Config.MDP_LOCATION_FOLDER)
+
+    # Get a list of the files in the MDP folder
+    list_files_mdp = os.listdir(folder_mdp)
+
     folder_run = os.path.abspath(os.path.join(folder, "run"))
     file_log_path = os.path.abspath(os.path.join(folder_run, "logs", "gmx.log"))
     file_step_path = os.path.abspath(os.path.join(folder, "steps.txt"))
     file_is_running = os.path.abspath(os.path.join(folder, "..", "is-running"))
     file_pid_path = os.path.join(folder_run, "pid_file")
+
+    # Copy each file to our run folder
+    for file in list_files_mdp:
+        file_path = os.path.join(folder_mdp, file)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, folder_run)
+
+    # UPDATE STATUS ON DB THAT `dynamic_id` is now running
+
+    # Load commands file contents to  a variable
+    file_commands_path = os.path.join(folder, "commands.txt")
+    with open(file_commands_path, "r") as f:
+        commands_file_content = f.readlines()
+
+    # Make each line of the file as a item in the excution array
+    commands = [line.rstrip("\n") for line in commands_file_content if line != "\n"]
 
     # Make our shell go to the run folder
     os.chdir(folder_run)
--- apps/server/server/resources/run/__init__.py
+++ apps/server/server/resources/run/__init__.py
@@ -9,39 +9,14 @@ from server.config import Config
 class RunDynamic(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument("folder_run", required=True, type=str, location="form")
+        parser.add_argument("folder", required=True, type=str, location="form")
 
         args = parser.parse_args()
 
         # Get absolute path to run folder
-        folder_run = os.path.abspath(args["folder_run"])
+        folder = os.path.abspath(args["folder"])
 
-        # Get absolute path to dynamic folder
-        folder = os.path.abspath(os.path.join(folder_run, ".."))
-
-        # Get absolute path to the folder where our default MDP files are stored
-        folder_mdp = os.path.abspath(Config.MDP_LOCATION_FOLDER)
-
-        # Get a list of the files in the MDP folder
-        list_files_mdp = os.listdir(folder_mdp)
-
-        # Copy each file to our run folder
-        for file in list_files_mdp:
-            file_path = os.path.join(folder_mdp, file)
-            if os.path.isfile(file_path):
-                shutil.copy(file_path, folder_run)
-
-        # UPDATE STATUS ON DB THAT `dynamic_id` is now running
-
-        # Load commands file contents to  a variable
-        file_commands_path = os.path.join(folder, "commands.txt")
-        with open(file_commands_path, "r") as f:
-            commands_file_content = f.readlines()
-
-        # Make each line of the file as a item in the excution array
-        commands = [line.rstrip("\n") for line in commands_file_content if line != "\n"]
-
-        run_commands.delay(folder, commands)
+        run_commands.delay(folder)
 
         # Return a response indicating that the command has started
         return {"status": "started"}
