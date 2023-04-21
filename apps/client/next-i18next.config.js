5021b51eeb46064379e19152b58958d7f401d2f8
Ivo <ivo@vist.gg>
2023-04-21 15:45:35 -0400

    fix: translations

--- apps/client/next-i18next.config.js
+++ apps/client/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en-US",
     locales: ["en-US", "pt-BR"],
+    localePath: path.resolve("./public/locales"),
     reloadOnPrerender: process.env.NODE_ENV === "development"
   },
   react: { useSuspense: false }
--- apps/client/src/middleware.ts
+++
@@ -1,5 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: ["/my-dynamics", "/dynamic/:path*", "/api/downloads/:path*"]
-};
