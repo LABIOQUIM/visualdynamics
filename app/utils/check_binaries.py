2559db5b602c44a9ca518b427efb080ae1bac8c3
Ivo <ivo@vist.gg>
2023-04-27 14:42:29 -0400

    fix: temporarily disable gmx_d detection

--- app/utils/check_binaries.py
+++ app/utils/check_binaries.py
@@ -2,10 +2,7 @@ import shutil
 
 
 def check_gromacs(double=False):
-    if double:
-        gmx = shutil.which("gmx_d") or shutil.which("gmx")
-    else:
-        gmx = shutil.which("gmx")
+    gmx = shutil.which("gmx")
     if gmx is None:
         return "missing_gromacs"
     return gmx
