2ea6a660f46af0b85acd8c919b9e44659823a052
Ivo <ivo@vist.gg>
2023-05-02 15:38:18 -0400

    chore: for tests

--- apps/client/src/lib/api.ts
+++ apps/client/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`
+  baseURL: `http://157.86.210.81:5000/api/v1`
+  // baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`
 });
