976fd15904f5f28f27331b71f5216fb2af87c052
Ivo <ivo@vist.gg>
2023-05-02 15:07:12 -0400

    chore: pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name: 'nextjs-client',
+    script: 'pnpm',
+    args:"start",
+    cwd:"~/visualdynamics2/apps/client",
+    instances: 2,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      APP_URL: "",
+      NEXT_PUBLIC_BACKEND_URL: '',
+      NEXTAUTH_URL: "",
+      NEXTAUTH_SECRET: "",
+      EMAIL_NO_REPLY: "",
+      EMAIL_PASS: ""
+    }
+  }]
+};
