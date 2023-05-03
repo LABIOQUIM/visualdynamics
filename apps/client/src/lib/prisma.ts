1cfa7c5bd7df3ee5fab263fc5534a9dca9021348
Ivo <ivo@vist.gg>
2023-05-02 21:17:16 -0400

    fix(client): disable prisma log query

--- apps/client/src/lib/prisma.ts
+++ apps/client/src/lib/prisma.ts
@@ -4,10 +4,6 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"]
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
