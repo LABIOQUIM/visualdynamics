d5be1d6fcb47ab04869fcf6d5f5a9dc3ec5945e3
Ivo <ivoprovensi1@gmail.com>
2023-06-07 19:08:23 -0400

    fix: use full baseurl if not development

--- apps/client/src/lib/api.tsx
+++ apps/client/src/lib/api.tsx
@@ -4,5 +4,5 @@ export const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:5000/api/v1"
-      : "/server/api/v1"
+      : "https://visualdynamics.fiocruz.br/server/api/v1"
 });
