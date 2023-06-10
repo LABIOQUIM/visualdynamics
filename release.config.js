5f3f97f78ee6ffde7e6cd6afd55c9f1a64faf0c4
Ivo <ivoprovensi1@gmail.com>
2023-06-10 12:55:22 -0400

    ci: go back to pat

--- release.config.js
+++ release.config.js
@@ -57,7 +57,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["apps/client/package.json", "yarn.lock", "CHANGELOG.md"],
+        assets: ["apps/client/package.json", "pnpm-lock.yaml", "CHANGELOG.md"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
