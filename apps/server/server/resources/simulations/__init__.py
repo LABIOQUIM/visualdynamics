4ab1df87bd362c2f501d7eb31590c45803b2c0d4
Ivo <ivoprovensi1@gmail.com>
2023-06-13 11:50:51 -0400

    fix: rename dynamics to simulations, properly setup cors

--- apps/server/server/resources/simulations/__init__.py
+++ apps/server/server/resources/simulations/__init__.py
@@ -1,10 +1,10 @@
 import os
 from flask import request
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from server.config import Config
 
 
-class UserDynamics(Resource):
+class UserSimulations(Resource):
     def get(self):
         args = request.args
 
@@ -12,31 +12,33 @@ class UserDynamics(Resource):
             os.path.join(Config.UPLOAD_FOLDER, args["username"])
         )
 
-        file_dynamics_list = os.path.abspath(os.path.join(folder_user, "dynamics.list"))
+        file_simulations_list = os.path.abspath(
+            os.path.join(folder_user, "dynamics.list")
+        )
 
         file_is_running = os.path.abspath(os.path.join(folder_user, "is-running"))
 
-        running_dynamic = ""
+        running_simulation = ""
 
         if os.path.isfile(file_is_running):
             with open(file_is_running, "r") as f:
-                running_dynamic = f.readline()
+                running_simulation = f.readline()
 
-        if not os.path.exists(file_dynamics_list):
+        if not os.path.exists(file_simulations_list):
             return {"status": "no-simulations"}
 
-        with open(file_dynamics_list, "r") as f:
-            dynamics_list = f.readlines()
+        with open(file_simulations_list, "r") as f:
+            simulations_list = f.readlines()
 
-        dynamics_list = dynamics_list[-10:]
+        simulations_list = simulations_list[-10:]
 
-        returnable_dynamics_list = []
+        returnable_simulations_list = []
 
-        for dynamic in dynamics_list:
-            extractable_data = dynamic.split("/")
+        for simulation in simulations_list:
+            extractable_data = simulation.split("/")
 
             file_celery_id = os.path.abspath(
-                os.path.join(dynamic.replace("\n", ""), "celery_id")
+                os.path.join(simulation.replace("\n", ""), "celery_id")
             )
 
             if os.path.exists(file_celery_id):
@@ -46,7 +48,7 @@ class UserDynamics(Resource):
                 celery_id = "unknown"
 
             file_status_path = os.path.abspath(
-                os.path.join(dynamic.replace("\n", ""), "status")
+                os.path.join(simulation.replace("\n", ""), "status")
             )
 
             if os.path.exists(file_status_path):
@@ -61,16 +63,19 @@ class UserDynamics(Resource):
 
             extractable_data = extractable_data[::-1]
 
-            dynamic_data = {
+            simulation_data = {
                 "timestamp": extractable_data[0].replace("\n", ""),
                 "type": extractable_data[2],
                 "molecule": extractable_data[1],
                 "celeryId": celery_id,
-                "isRunning": dynamic.replace("\n", "") == running_dynamic,
+                "isRunning": simulation.replace("\n", "") == running_simulation,
                 "status": status,
                 "errored_command": errored_command if status == "error" else None,
             }
 
-            returnable_dynamics_list.append(dynamic_data)
+            returnable_simulations_list.append(simulation_data)
 
-        return {"status": "has-simulations", "simulations": returnable_dynamics_list[::-1]}
+        return {
+            "status": "has-simulations",
+            "simulations": returnable_simulations_list[::-1],
+        }
