9935fecc433e835fc18ff09393d6e690984cbf1e
Ivo <ivoprovensi1@gmail.com>
2023-06-18 19:36:14 -0400

    fix: add publications section on about page

--- apps/client/src/pages/about/index.tsx
+++ apps/client/src/pages/about/index.tsx
@@ -1,8 +1,11 @@
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
 import useTranslation from "next-translate/useTranslation";
 
 import { MaintainerCard } from "@app/components/about/maintainer-card";
 import { BlurImage } from "@app/components/general/blur-image";
 import { PageLayout } from "@app/components/general/page-layout";
+import { H1, H2 } from "@app/components/general/typography/headings";
 import { SEO } from "@app/components/seo";
 
 import logo from "../../../public/images/favicon.svg";
@@ -68,16 +71,32 @@ export default function About() {
           {t("about:acknowledgements")}
         </p>
 
+        <div>
+          <H1>{t("about:publication.title")}</H1>
+
+          <ul className="list-[upper-roman] leading-relaxed">
+            <li>
+              Visual dynamics: a WEB application for molecular dynamics
+              simulation using GROMACS. BMC Bioinformatics 24, 107 (2023).
+              <Link
+                className="inline-block underline underline-offset-4 hover:opacity-75"
+                href="https://doi.org/10.1186/s12859-023-05234-y"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://doi.org/10.1186/s12859-023-05234-y
+                <ExternalLink className="ml-0.5 inline-block h-4 w-4" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+
         <div className="flex flex-col gap-y-3">
-          <h3 className="text-xl font-bold uppercase text-primary-950 dark:text-primary-400">
-            {t("about:maintainers.title")}
-          </h3>
+          <H1>{t("about:maintainers.title")}</H1>
 
           <div className="flex flex-col gap-y-2">
-            <h5 className="text-lg font-medium uppercase text-primary-900 dark:text-primary-300">
-              {t("about:maintainers.active")}
-            </h5>
-            <div className="flex flex-wrap justify-between gap-x-2">
+            <H2>{t("about:maintainers.active")}</H2>
+            <div className="grid grid-flow-row grid-cols-2 gap-2 lg:grid-cols-3">
               {maintainers
                 .filter((maintainer) => maintainer.active)
                 .map((maintainer) => (
@@ -90,10 +109,8 @@ export default function About() {
           </div>
 
           <div className="flex flex-col gap-y-2">
-            <h5 className="text-lg font-medium uppercase text-primary-900 dark:text-primary-300">
-              {t("about:maintainers.inactive")}
-            </h5>
-            <div className="flex flex-wrap justify-between gap-x-2">
+            <H2>{t("about:maintainers.inactive")}</H2>
+            <div className="grid grid-flow-row grid-cols-2 gap-2 lg:grid-cols-3">
               {maintainers
                 .filter((maintainer) => !maintainer.active)
                 .map((maintainer) => (
