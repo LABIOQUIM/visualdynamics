d73407f7210791905ae9000f6a49b65f43b9610b
Ivo <ivoprovensi1@gmail.com>
2023-06-30 15:05:50 -0400

    fix(server): return all worker tasks

--- apps/server/server/resources/celery/reserved_tasks.py
+++ apps/server/server/resources/celery/reserved_tasks.py
@@ -4,4 +4,4 @@ from server.celery import celery
 
 class CeleryReservedTasks(Resource):
     def get(self):
-        return celery.control.inspect(["worker@visualdynamics"]).reserved()
+        return celery.control.inspect().reserved()
