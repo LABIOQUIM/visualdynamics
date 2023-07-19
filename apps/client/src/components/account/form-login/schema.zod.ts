af65eb3115e0a4d17f7792d89f467694c4bf1c10
Ivo <ivoprovensi1@gmail.com>
2023-07-19 14:22:41 -0400

    fix: tutorials and posts page on pt-br not working

--- apps/client/src/components/account/form-login/schema.zod.ts
+++ apps/client/src/components/account/form-login/schema.zod.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const SignInFormSchema = z.object({
-  identifier: z.string().min(1, "account-loginidentifier.errors.empty"),
-  password: z.string().min(1, "account-loginpassword.errors.empty")
+  identifier: z.string().min(1, "account-login:identifier.errors.empty"),
+  password: z.string().min(1, "account-login:password.errors.empty")
 });
 
 export type SignInFormSchemaType = z.infer<typeof SignInFormSchema>;
