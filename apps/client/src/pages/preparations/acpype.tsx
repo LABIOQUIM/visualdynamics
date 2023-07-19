56995dd76aac602cbc0b0ef061790f0fbe420884
Ivo <ivoprovensi1@gmail.com>
2023-07-19 11:38:55 -0400

    fix: rename posts page and update contents of limitations

--- apps/client/src/pages/preparations/acpype.tsx
+++ apps/client/src/pages/preparations/acpype.tsx
@@ -4,7 +4,7 @@ import { H1 } from "@app/components/general/typography/headings";
 export default function LigandACPYPE() {
   return (
     <PageLayout>
-      <H1>WIP</H1>
+      <H1>Work in Progress</H1>
     </PageLayout>
   );
 }
