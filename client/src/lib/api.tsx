8c58865c9959e6576fe05de304d6512e8ff6a263
Ivo <ivoprovensi1@gmail.com>
2023-08-07 18:12:49 -0400

    feat(infra): introduce traefik

--- client/src/lib/api.tsx
+++ client/src/lib/api.tsx
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:3002/api/v1"
-      : "https://visualdynamics.fiocruz.br/server/api/v1"
+  baseURL: `/server/api/v1`
 });
