697c333288e0bf55ceff898bc8dd5ea29e6c4b3b
Ivo <ivoprovensi1@gmail.com>
2023-08-10 16:21:34 -0400

    feat: build gromacs docker image locally

--- server/server/config.py
+++ server/server/config.py
@@ -4,13 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    GMX_NT = 10
     SECRET_KEY = os.environ.get("VISUAL_DYNAMICS_SECRET_KEY")
     UPLOAD_FOLDER = os.path.abspath("/var/www/VDfiles")
     STATIC_FOLDER = os.path.abspath(os.path.join(basedir, "static"))
     MDP_LOCATION_FOLDER = (
         os.path.abspath(os.path.join(STATIC_FOLDER, "mdpdev"))
-        if os.environ.get("FLASK_DEBUG")
+        if os.environ.get("FLASK_DEBUG") == 1
         else os.path.abspath(os.path.join(STATIC_FOLDER, "mdp"))
     )
     SOCK_SERVER_OPTIONS = {"ping_interval": 25}
