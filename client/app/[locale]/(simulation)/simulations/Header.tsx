aaa2e6b2f198aacf4af3cdb606150ac88d192cf9
Ivo <ivoprovensi1@gmail.com>
2023-08-23 10:08:54 -0400

    fix(client): fix eslint warnings

--- client/app/[locale]/(simulation)/simulations/Header.tsx
+++ client/app/[locale]/(simulation)/simulations/Header.tsx
@@ -1,9 +1,7 @@
-import { FileDown, RefreshCw } from "lucide-react";
-import NextLink from "next/link";
+import { RefreshCw } from "lucide-react";
 
 import { DownloadMDP } from "@/app/[locale]/(simulation)/simulations/DownloadMDP";
 import { Button } from "@/components/Button";
-import { useI18n } from "@/locales/client";
 import { cnMerge } from "@/utils/cnMerge";
 
 interface MyDynamicsHeader {
@@ -13,8 +11,6 @@ interface MyDynamicsHeader {
 }
 
 export function Header({ refetch, isLoading, isRefetching }: MyDynamicsHeader) {
-  const t = useI18n();
-
   return (
     <div className="flex flex-col gap-3 md:flex-row">
       <DownloadMDP />
