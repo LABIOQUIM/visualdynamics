6d947fe917bf9c40cbed2d32a8a57a7022cbba35
Ivo <ivoprovensi1@gmail.com>
2025-07-20 09:04:00 -0400

    refactor(navigation): update links and titles in AdministrationNav component

--- apps/web/components/Administration/Nav.tsx
+++ apps/web/components/Administration/Nav.tsx
@@ -3,7 +3,6 @@ import { Box } from "@mantine/core";
 import {
   IconLayoutList,
   IconReportAnalytics,
-  IconServer,
   IconSettings2,
   IconUsers,
 } from "@tabler/icons-react";
@@ -14,65 +13,27 @@ import classes from "./Nav.module.css";
 
 const nav: NavSection[] = [
   {
-    title: "LABIOQUIM",
-    links: [
-      {
-        href: "/administration/users",
-        icon: IconUsers,
-        label: "Users",
-      },
-      {
-        href: "/administration/server",
-        icon: IconServer,
-        disabled: true,
-        label: "Server Manager",
-      },
-      {
-        href: "/administration/settings",
-        icon: IconSettings2,
-        label: "App Settings",
-      },
-    ],
-  },
-  {
     title: "Visual Dynamics",
     links: [
       {
-        href: "/administration/visualdynamics/status",
+        href: "/dashboard/administration/users",
+        icon: IconUsers,
+        label: "Registered Users",
+      },
+      {
+        href: "/dashboard/administration/visualdynamics/status",
         icon: IconReportAnalytics,
-        label: "Status",
+        label: "Server and Queue Status",
       },
       {
-        href: "/administration/visualdynamics/manage",
+        href: "/dashboard/administration/visualdynamics/manage",
         icon: IconLayoutList,
-        label: "Manage",
+        label: "Manage Simulations",
       },
       {
-        href: "/administration/visualdynamics/settings",
+        href: "/dashboard/administration/visualdynamics/settings",
         icon: IconSettings2,
         label: "Settings",
-      },
-    ],
-  },
-  {
-    title: "PlasmoQSAR",
-    disabled: true,
-    links: [
-      {
-        href: "/administration/qsar",
-        icon: IconReportAnalytics,
-        label: "Analytics",
-      },
-    ],
-  },
-  {
-    title: "PlasmoIA",
-    disabled: true,
-    links: [
-      {
-        href: "/administration/simulations",
-        icon: IconReportAnalytics,
-        label: "Analytics",
       },
     ],
   },
