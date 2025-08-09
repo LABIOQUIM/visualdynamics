4f8591b3595b0f6f77ddf9c369351f68469902c7
Ivo <ivoprovensi1@gmail.com>
2025-08-09 13:12:49 -0400

    feat(release): add semantic release configuration
    * Configure branches for main and beta releases
    * Set up plugins for version replacement and release notes generation
    * Include assets for versioning in package.json and changelog

---
+++ release.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  branches: ["main", { name: "next", channel: "beta", prerelease: true }],
+  plugins: [
+    [
+      "semantic-release-replace-plugin",
+      {
+        replacements: [
+          {
+            files: ["package.json", "apps/api/package.json", "apps/web/package.json"],
+            from: "\"version\": \".*\"", // eslint-disable-line
+            to: "\"version\": \"${nextRelease.version}\"", // eslint-disable-line
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              type: "feat",
+              section: ":sparkles: Features",
+              hidden: false,
+            },
+            {
+              type: "fix",
+              section: ":bug: Fixes",
+              hidden: false,
+            },
+            {
+              type: "docs",
+              section: ":memo: Documentation",
+              hidden: false,
+            },
+            {
+              type: "refactor",
+              section: ":zap: Refactorings",
+              hidden: false,
+            },
+            {
+              type: "ci",
+              section: ":repeat: CI",
+              hidden: false,
+            },
+            {
+              type: "chore",
+              section: ":broom: Chore",
+              hidden: false,
+            },
+          ],
+        },
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["apps/api/package.json", "apps/web/package.json", "package.json", "yarn.lock", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}", // eslint-disable-line
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
