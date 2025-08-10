89988c2d640c2da249dce3ee8c72e59db6ff3a75
Ivo <ivoprovensi1@gmail.com>
2025-08-10 13:54:03 -0400

    style(page): remove unnecessary blank line in My Simulations component

--- apps/web/app/[locale]/app/(home)/page.tsx
+++ apps/web/app/[locale]/app/(home)/page.tsx
@@ -19,7 +19,6 @@ export default function Page() {
           title="My Simulations"
           rightElement={<NewSimulationButton />}
         />
-
         <MySimulations />
       </div>
     </PageLayout>
